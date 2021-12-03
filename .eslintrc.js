834c1bd1ac48746fd816b2dafda2518204140f20
Shailesh Savaliya <sbthemes@gmail.com>
2021-12-03 12:57:07 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,6 @@ module.exports = {
     plugins: [],
     // add your custom rules here
     rules: {
-        'max-len': [2, { code: 120, tabWidth: 4, ignoreUrls: true }],
+        'vue/max-len': ['error', { code: 120, tabWidth: 4, ignoreHTMLAttributeValues: true }],
     },
-}
+};
